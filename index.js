529e12a862f9fe806a184d3f089e5f28a6fc87ae
Monpress1 <elimonpresss@gmail.com>
2025-02-13 18:03:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // server.js
 const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 12345 });
+const server = new WebSocket.Server({ port: 'https://websocket-server-6jct.onrender.com' });
 server.on('connection', (socket) => {
     console.log('A new client connected!');
     // Listen for messages from clients
